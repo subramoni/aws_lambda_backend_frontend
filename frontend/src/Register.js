1bb861ff50c329b450cd536b6d1630ff196668a3
subramoni <99260837+subramoni@users.noreply.github.com>
2022-12-27 20:32:55 +0530

    I have got error in adding api key in axios. This error is rectified

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -1,18 +1,52 @@
 import React from 'react'
 import { useState } from 'react'
+import axios from 'axios';
+
+const registerUrl = 'https://ngp11dkhdl.execute-api.us-east-1.amazonaws.com/dev/register?';
 
 const Register = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
 
   const submitHandler = (event) => {
     event.preventDefault();
+
+    if(username.trim() === '' || email.trim() === '' || password.trim() === '')
+    {
+        setMessage('All fields are required');
+    }
+    
+    const requestConfig = {
+      headers : {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    const requestBody = {
+      username:username,
+      email:email,
+      name:name,
+      password:password
+    }
+
+    axios.defaults.headers.common = {
+      "X-API-Key": "XUeDCkuMZd7FEQr3DqYg07OHJBtSr21M9Whwgaad",
+    };
+    
+    axios.post(registerUrl,  requestBody, requestConfig).then(response => {
+      setMessage('Register successfully')
+    }).catch(error => {
+      if(error.response.status === 401){
+        setMessage(error.response.data.message)
+      }
+      setMessage('Sorry... the backend server is down!! please try again later')
+      console.log(error)
+    });
     
   }
-
-
   return (
     <div>
       <form onSubmit={submitHandler}>
@@ -22,6 +56,7 @@ const Register = () => {
         password : <input type='text' value={password} onChange={event => setPassword(event.target.value)}/><br/><br/>
         <input type='submit'/>
       </form>
+      <p className='message'>{message}</p>
     </div>
   )
 }
