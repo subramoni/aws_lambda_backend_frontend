d5dd8dfe04ed6c999d907fe5e3733d2cb40418d3
subramoni <99260837+subramoni@users.noreply.github.com>
2023-01-03 18:57:44 +0530

    Backend lambda 1st checking code added

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,64 @@
+aws lambda 1st checking
+
+const users = '/users';
+const user = '/user';
+const signup = '/signup';
+const login = '/login';
+
+exports.handler = async (event) => {
+    console.log('Request Event: ', event);
+    let response;
+    switch(true){
+        case event.httpMethod === 'GET' && event.path === users:
+            response = buildResponse(200);
+            break;
+        case event.httpMethod === 'GET' && event.path === user:
+            response = buildResponse(200);
+            break;
+        case event.httpMethod === 'POST' && event.path === signup:
+            response = buildResponse(200);
+            break;
+        case event.httpMethod === 'POST' && event.path === login:
+            response = buildResponse(200);
+            break;
+        default:
+            response = buildResponse(404, '404 not found')
+    }
+    return response;
+};
+
+function buildResponse(statusCode, body){
+    return {
+        statusCode: statusCode,
+        headers:{
+            'Access-Control-Allow-Origin':'*',
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify(body)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const registerService = require('./service/register');
 const loginService = require('./service/login');
 const verifyService = require('./service/verify');
